8641b91b10e892d16ea0f1e6b721e1a525985fbb
truelanz <devalanoliveira@gmail.com>
2025-06-03 12:37:39 -0300

    UserServiceTests

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -3,12 +3,9 @@ package com.devsuperior.dscommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +14,16 @@ import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
+import com.devsuperior.dscommerce.ultil.CustomUserUtil;
 
 @Service
 public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
+
+	@Autowired
+	private CustomUserUtil customUserUtil;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -44,9 +45,7 @@ public class UserService implements UserDetailsService {
 	
 	protected User authenticated() {
 		try {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-			String username = jwtPrincipal.getClaim("username");
+			String username = customUserUtil.getLoggedUsername();
 			return repository.findByEmail(username).get();
 		}
 		catch (Exception e) {
